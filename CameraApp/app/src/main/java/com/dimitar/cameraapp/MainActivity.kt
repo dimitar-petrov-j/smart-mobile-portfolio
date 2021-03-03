5db4439b1340f3d3583e27114aacb6070ae01903
Petrov,Dimitar D <d.petrov-ab@student.fontys.nl>
2021-03-03 13:50:39 +0200

    saving to gallery successful

--- CameraApp/app/src/main/java/com/dimitar/cameraapp/MainActivity.kt
+++ CameraApp/app/src/main/java/com/dimitar/cameraapp/MainActivity.kt
@@ -1,55 +1,68 @@
 package com.dimitar.cameraapp
 
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import java.io.ByteArrayOutputStream
+import com.google.firebase.storage.UploadTask
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val cameraRequest = 1888
+    private val REQUEST_IMAGE_CAPTURE = 1
     private var uri: Uri? = null
-    private var storage: FirebaseStorage? = null
     private var mStorageRef: StorageReference? = null
+    private var ref: StorageReference? = null
     lateinit var imageView: ImageView
     lateinit var uploadButton: Button
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == cameraRequest) {
-            val photo: Bitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(photo)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
             uploadButton.isVisible = true;
-            uri = getImageUri(applicationContext,photo);
+            createImageFile()
 
         }
     }
 
-    fun getImageUri(inContext: Context?, inImage: Bitmap): Uri? {
-        val bytes = ByteArrayOutputStream()
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val path = MediaStore.Images.Media.insertImage(
-            this@MainActivity.contentResolver,
-            inImage,
-            UUID.randomUUID().toString() + ".png",
-            "drawing"
-        )
-        return Uri.parse(path)
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,21 +72,50 @@ class MainActivity : AppCompatActivity() {
 
         if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_DENIED)
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), cameraRequest)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), REQUEST_IMAGE_CAPTURE)
         imageView = findViewById(R.id.imageView)
         uploadButton = findViewById(R.id.buttonUpload)
         val photoButton: Button = findViewById(R.id.button)
 
         photoButton.setOnClickListener {
 
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(cameraIntent, cameraRequest)
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                // Ensure that there's a camera activity to handle the intent
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                                this,
+                                "com.dimitar.cameraapp",
+                                it
+                        )
+                        uri = photoURI
+                    }
+                }
+            }
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
         }
 
         uploadButton.setOnClickListener {
+            ref = mStorageRef!!.child("images/"+ UUID.randomUUID().toString())
+            val uploadTask = ref?.putFile(uri!!)
 
-            mStorageRef!!.putFile(uri!!)
-
+            val urlTask = uploadTask?.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation ref!!.downloadUrl
+            })
         }
     }
 }
