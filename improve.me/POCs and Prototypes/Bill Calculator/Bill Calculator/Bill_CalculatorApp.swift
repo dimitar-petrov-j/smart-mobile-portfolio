c0a3d01310028ad84cad2ad7177b59b129399367
Dimitar Petrov <425575@student.fontys.nl>
2021-03-24 03:17:00 -0700

    Bill Calculator app

---
+++ improve.me/POCs and Prototypes/Bill Calculator/Bill Calculator/Bill_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_CalculatorApp.swift
+//  Bill Calculator
+//
+//  Created by user193700 on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Bill_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
