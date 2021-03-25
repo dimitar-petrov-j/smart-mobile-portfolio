f451bdbab0c560518efe8767cccf23bf4b1a474c
Dimitar Petrov <425575@student.fontys.nl>
2021-03-25 08:03:22 -0700

    App using ML to calculate when you should sleep

---
+++ improve.me/POCs and Prototypes/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by user193700 on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
