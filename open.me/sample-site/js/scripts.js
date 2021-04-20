9356a0d1c2da1fdbbf66590a43d945cc37a5f8d5
Petrov,Dimitar D <d.petrov-ab@student.fontys.nl>
2021-04-20 15:42:05 +0300

    push notifications

--- open.me/sample-site/js/scripts.js
+++ open.me/sample-site/js/scripts.js
@@ -10,4 +10,27 @@ if("serviceWorker" in navigator){
   } else {
     //The registration of the service worker failed
     console.log("Browser: I don't support Service Workers :(");
-  }
+}
+
+//Asking for permission with the Notification API
+if(typeof Notification!==typeof undefined){ //First check if the API is available in the browser
+	Notification.requestPermission().then(function(result){ 
+		//If accepted, then save subscriberinfo in database
+		if(result==="granted"){
+			console.log("Browser: User accepted receiving notifications, save as subscriber data!");
+			navigator.serviceWorker.ready.then(function(serviceworker){ //When the Service Worker is ready, generate the subscription with our Serice Worker's pushManager and save it to our list
+				const VAPIDPublicKey="BO0BNVXEyAVXiK08jLxFPM1M7UfJ2KJfq9KXq92UqoSIkEkRDFDt1z_SGbZ7E6ojGpd3SJpXXJrWxHIveBN4Rpk"; // Fill in your VAPID publicKey here
+				const options={applicationServerKey:VAPIDPublicKey,userVisibleOnly:true} //Option userVisibleOnly is neccesary for Chrome
+				serviceworker.pushManager.subscribe(options).then((subscription)=>{
+          //POST the generated subscription to our saving script (this needs to happen server-side, (client-side) JavaScript can't write files or databases)
+					let subscriberFormData=new FormData();
+					subscriberFormData.append("json",JSON.stringify(subscription));
+					fetch("data/saveSubscription.php",{method:"POST",body:subscriberFormData}).then((success)=>{ console.log(success)}).catch((error)=>{ console.log(error)});
+				});
+			});
+		}
+	}).catch((error)=>{
+		console.log(error);
+	});
+}
+ 
